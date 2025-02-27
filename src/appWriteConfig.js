daf764e881456813ad0eede61c7063ccc7587439
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-02-27 23:35:07 +0100

    add routing and authentication features with private routes

--- src/appWriteConfig.js
+++ src/appWriteConfig.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from 'appwrite';
+import { Client, Databases, Account } from 'appwrite';
 
 export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
@@ -11,5 +11,6 @@ const client = new Client();
 client.setEndpoint(API_ENDPOINT).setProject(PROJECT_ID);
 
 export const databases = new Databases(client);
+export const account = new Account(client);
 
 export default client;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import client, { databases, DATABASE_ID, COLLECTION_ID_MESSAGES } from '../appWriteConfig'
-import { ID, Query } from 'appwrite'
+import { ID, Query, Role, Permission } from 'appwrite'
 import { Trash2 } from 'react-feather'
+import Header from '../components/Header'
+import { userAuth } from '../utils/AuthContext'
 
 const Room = () => {
+
+    const { user } = userAuth()
 
     const [messages, setMessages] = useState([])
     const [messageBody, setMessageBody] = useState('')
@@ -36,14 +40,21 @@ const Room = () => {
         e.preventDefault();
 
         let payload = {
+            user_id: user.$id,
+            username: user.name,
             body: messageBody
         }
+
+        let permissions = [
+            Permission.write(Role.user(user.$id))
+        ]
 
         let response = await databases.createDocument(
             DATABASE_ID,
             COLLECTION_ID_MESSAGES,
             ID.unique(),
-            payload
+            payload,
+            permissions
         )
 
         console.log('Created!', response)
@@ -73,48 +84,60 @@ const Room = () => {
     }
 
     return (
-        <div className='container'>
-
-            <form onSubmit={handleSubmit} id='message--form'>
-                <div>
-                    <textarea
-                        required
-                        maxLength='1000'
-                        placeholder='Say something...'
-                        value={messageBody}
-                        onChange={(e) => setMessageBody(e.target.value)}
-                        name="body"
-                    ></textarea>
-                </div>
-
-                <div className='send-btn--wrapper'>
-                    <input className='btn btn--secondary' type="submit" value='send' />
-                </div>
-            </form>
-
+        <main className='container'>
+            <Header />
             <div className="room--container">
+                <form onSubmit={handleSubmit} id='message--form'>
+                    <div>
+                        <textarea
+                            required
+                            maxLength='1000'
+                            placeholder='Say something...'
+                            value={messageBody}
+                            onChange={(e) => setMessageBody(e.target.value)}
+                            name="body"
+                        ></textarea>
+                    </div>
+
+                    <div className='send-btn--wrapper'>
+                        <input className='btn btn--secondary' type="submit" value='send' />
+                    </div>
+                </form>
+
                 <div>
                     {messages.map(message => (
                         <div key={message.$id} className='message--wrapper'>
 
                             <div className='message--header'>
-                                <small className='message-timestamp'>{new Date(message.$createdAt).toLocaleString()}</small>
 
-                                <Trash2
-                                    className='delete--btn'
-                                    onClick={() => { deleteMessage(message.$id) }}
-                                />
+                                <p>
+                                    {message?.username ? (
+                                        <span>{message.username}</span>
+                                    ) : (
+                                        <span>Anonymous user</span>
+                                    )}
+
+                                    <small className='message-timestamp'>{new Date(message.$createdAt).toLocaleString()}</small>
+                                </p>
+
+                                {message.$permissions.includes(`delete("user:${user.$id}")`) && (
+                                    <Trash2
+                                        className='delete--btn'
+                                        onClick={() => { deleteMessage(message.$id) }}
+                                    />
+                                )}
+
                             </div>
 
-                            <div className='message--body'>
+                            <div className={"message--body" + (message.user_id === user.$id ? ' message--body--owner' : '')}>
                                 <span>{message.body}</span>
+
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
-
-        </div>
+        </main>
     )
 }
 
